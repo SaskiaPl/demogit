4d82bb8859fbf0e14a74df6da3ad02d0c5527e97
SaskiaPl <saskia.pluester@accenture.com>
2019-09-11 11:13:36 +0200

    Destroy

--- demo/src/test/java/com/example/demo/SaskiaGitApplicationTests.java
+++ demo/src/test/java/com/example/demo/SaskiaGitApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.hazelcast.HazelcastClientFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,5 +13,6 @@ public class SaskiaGitApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
-
+HazelcastClientFactory{nbcwdjifhw
+I am testing git versions to make it kaputt
 }
